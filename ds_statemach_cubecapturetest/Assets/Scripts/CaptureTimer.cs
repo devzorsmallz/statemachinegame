89741460a90e7d8469f87ad27e706db378f30c12
devzorsmallz <46795817+devzorsmallz@users.noreply.github.com>
2020-08-31 15:19:48 -0600

    Added Cube Capture Test
    Basic Roll a Ball game with "capture time" for cube collectibles.

---
+++ ds_statemach_cubecapturetest/Assets/Scripts/CaptureTimer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureTimer : MonoBehaviour
+{
+    public bool playerHasCaptured = false;
+    public GameObject player;
+
+    private bool playerCapturing = false;
+    private bool hasBeenCounted = false;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<PlayerController>().captureTime > 0 && playerCapturing)
+        {
+            if (player.GetComponent<PlayerController>().captureTime > 0)
+            {
+                player.GetComponent<PlayerController>().captureTime--;
+                anim.SetBool("shrinkAnim", true);
+            }
+        }
+
+        if (player.GetComponent<PlayerController>().captureTime == 0 && hasBeenCounted == false && playerCapturing)
+        {
+            playerHasCaptured = true;
+            hasBeenCounted = true;
+            player.GetComponent<PlayerController>().count++;
+            player.GetComponent<PlayerController>().SetCountText();
+
+            Debug.Log("Cube " + player.GetComponent<PlayerController>().count + " Captured!");
+            player.GetComponent<PlayerController>().captureTime = 100;
+
+            player.GetComponent<ChuckSubInstance>().RunCode(@"
+			SinOsc foo => dac;
+			repeat( 15 )
+			{
+				Math.random2f( 300, 700 ) => foo.freq;
+				10::ms => now;
+			}
+		    ");
+
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerCapturing = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerCapturing = false;
+            player.GetComponent<PlayerController>().captureTime = 100;
+            anim.SetBool("shrinkAnim", false);
+        }
+    }
+}
---
+++ ds_statemach_cubecapturetest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public int captureTime = 100;
+    public int count = 0;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    public void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
